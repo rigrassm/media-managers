155263e4fe27ae05777eb856921d203d04468edc
Ricky <rigrassm@gmail.com>
2016-05-01 01:18:09 -0500

    Initial Commit - Combining of multiple related packages

---
+++ package.js
@@ -0,0 +1,59 @@
+Package.describe({
+  name: 'rigrassm:mediaManagers',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: 'Wrapper for media managing applications used by Plexrequests',
+  // URL to the Git repository containing the source code for this package.
+  git: 'https://github.com/rigrassm/mediaManagers',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.3');
+
+  //Couchpotato
+
+  api.addFiles('lib/server/methods/couchpotato/appAvailable.js', ['server']);
+  api.addFiles('lib/server/methods/couchpotato/mediaGet.js', ['server']);
+  api.addFiles('lib/server/methods/couchpotato/movieAdd.js', ['server']);
+  api.addFiles('lib/server/methods/couchpotato/movieDelete.js', ['server']);
+
+  // Sickrage
+
+  api.addFiles('lib/server/methods/tvmaze.js', ['server']);
+  api.addFiles('lib/server/methods/sickrage/available.js', ['server']);
+  api.addFiles('lib/server/methods/sickrage/checkShow.js', ['server']);
+  api.addFiles('lib/server/methods/sickrage/addShow.js', ['server']);
+  api.addFiles('lib/server/methods/sickrage/deleteShow.js', ['server']);
+  api.addFiles('lib/server/methods/sickrage/statsShow.js', ['server']);
+
+  // Sonarr
+  
+  api.addFiles('lib/server/methods/sonarr/systemStatus.js', ['server']);
+  api.addFiles('lib/server/methods/sonarr/profilesGet.js', ['server']);
+  api.addFiles('lib/server/methods/sonarr/seriesStats.js', ['server']);
+  api.addFiles('lib/server/methods/sonarr/seriesPost.js', ['server']);
+  api.addFiles('lib/server/methods/sonarr/seriesGet.js', ['server']);
+  api.addFiles('lib/server/methods/sonarr/seriesDelete.js', ['server']);
+  
+  
+  api.use('meteor-base@1.0.1')
+  api.use('templating');
+  api.use('underscore');
+  api.use('check');
+  api.use('http');
+  api.use('reactive-var');
+  api.use('fortawesome:fontawesome@4.4.0')
+
+  api.export("mediaManagers");
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('rigrassm:mediaManagers');
+  api.addFiles('mediaManagers-tests.js');
+});
+
+
