3c089f2919746c2b99ae3f32d5f65dd428129513
Ricky <rigrassm@gmail.com>
2016-05-05 23:57:12 -0500

    version update

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'rigrassm:media-managers',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'Wrapper for media managing applications used by Plexrequests',
   // URL to the Git repository containing the source code for this package.
